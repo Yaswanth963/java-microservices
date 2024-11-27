a88ad75e5bd2be24364c69ccbf6528fbc38cad64
Yaswanth963 <perumallayaswanth2000@gmail.com>
2024-11-27 23:01:23 +0530

    Added Inventory Service, Product Service and Eureka server

---
+++ productService/src/main/java/com/microservices/productService/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.microservices.productService.controller;
+
+import com.microservices.productService.entity.Product;
+import com.microservices.productService.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
---
+++ productService/src/main/java/com/microservices/productService/entity/Product.java
@@ -0,0 +1,19 @@
+package com.microservices.productService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+@Entity
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+}
